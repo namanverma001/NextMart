f7d6a0731b35c5fd11452a44a1e3966931967bff
namanverma001 <namanverma0219@gmail.com>
2024-12-13 12:41:56 +0530

    2nd update

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const color = require('colors');
-//rest object
-const app = express()
-//rest api
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js"
+//config env
+dotenv.config();
+connectDB();
+// Initialize express application
+const app = express();
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use("/api/v1/auth", authRoute);
+// Define a simple route
 app.get('/', (req, res) => {
-    resizeTo.send('<h1>welcome to app</h1>');
+    res.send('<h1>Welcome to NextMart</h1>'); // Corrected from resizeTo.send to res.send
 });
 
-const PORT = 8000
+// Define the server port
+const PORT = process.env.PORT || 8080;
+
+// Start the server
 app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`.bgCyan.white);
+    console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
